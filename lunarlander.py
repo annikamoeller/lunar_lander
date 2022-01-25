38b3dd44386a49dc2858e010849429d8c75d4eb0
Annika Möller Chandiramani <annika.moeller24@gmail.com>
2022-01-25 15:16:32 +0800

    return to previous

--- lunarlander.py
+++ lunarlander.py
@@ -12,7 +12,7 @@ from collections import deque
 
 from ReplayBuffer import ReplayBuffer
 from Experience import Experience
-from Utils import Logger, AverageRewardTracker, plot, train_loop, render_game
+from Utils import Logger, AverageRewardTracker, backup_model, plot, train_loop, render_game
 from DDQN import DDQN
 
 env = gym.make("LunarLander-v2")
@@ -38,7 +38,53 @@ backup_freq = 100
 agent = DDQN(state_space, action_space, learning_rate, 
   gamma, epsilon, min_epsilon, decay_rate, buffer_maxlen, reg_factor)
 
-model, progress_data = train_loop(agent, env, batch_size, training_start,
-  target_update_freq, max_episodes, max_steps, train_freq, backup_freq)
 
-plot(progress_data)
+# agent.load_from_weights('model_200.h5')
+
+avg_reward_tracker = AverageRewardTracker(100) 
+logger = Logger()
+
+for episode in range(max_episodes): # training loop
+  state = env.reset() # vector of 8
+
+  episode_reward = 0 # reward tracker
+
+  for step in range(max_steps): # limit number of steps
+    action = agent.select_action(state) # get action 
+    next_state, reward, done, info = env.step(action) # next step
+    episode_reward += reward # increment reward
+
+    if step == max_steps: # stop at max steps 
+      print(f"Episode reached the maximum number of steps. {max_steps}")
+      done = True
+
+    experience = Experience(state, action, reward, next_state, done) # create new experience object
+    agent.buffer.add(experience) # add experience to buffer
+
+    state = next_state # update state
+
+    if step % target_update_freq == 0: # update target weights every x steps 
+      print("Updating target model")
+      agent.update_target_weights()
+    
+    if (agent.buffer.length() >= training_start) & (step % train_freq == 0): # train agent every x steps
+      batch = agent.buffer.sample(batch_size)
+      inputs, targets = agent.calculate_inputs_and_targets(batch)
+      agent.train(inputs, targets)
+
+    if done: # stop if this action results in goal reached
+      break
+  
+  avg_reward_tracker.add(episode_reward)
+  average = avg_reward_tracker.get_average()
+
+  print(f"EPISODE {episode} finished in {step} steps, " )
+  print(f"epsilon {agent.epsilon}, reward {episode_reward}. ")
+  print(f"Average reward over last 100: {average} \n")
+  logger.log(episode, step, episode_reward, average)
+  if episode != 0 and episode % backup_freq == 0: # back up model every x steps 
+    backup_model(agent.model, episode)
+  
+  agent.epsilon_decay()
+
+plot(logger)
