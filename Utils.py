6b4fc5f1472aeccc0fae1e845519262ca00864b7
Annika Möller Chandiramani <annika.moeller24@gmail.com>
2022-01-25 15:04:22 +0800

    shuffled files around

--- Utils.py
+++ Utils.py
@@ -3,22 +3,25 @@ from collections import deque
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+import gym
+from Experience import Experience
 
-class FileLogger():
+class Logger():
 
-  def __init__(self, file_name='progress.log'):
+  def __init__(self, file_name='training_progress.log'):
     self.file_name = file_name
     self.clean_progress_file()
 
   def log(self, episode, steps, reward, average_reward):
-    f = open(self.file_name, 'a+')
+    f = open(self.file_name, 'a')
     f.write(f"{episode};{steps};{reward};{average_reward}\n")
     f.close()
 
   def clean_progress_file(self):
     if os.path.exists(self.file_name):
       os.remove(self.file_name)
-    f = open(self.file_name, 'a+')
+    f = open(self.file_name, 'a')
     f.write("episode;steps;reward;average\n")
     f.close()
 
@@ -35,13 +38,67 @@ class AverageRewardTracker():
   def get_average(self):
     return np.average(self.tracker)
 
+
+def train_loop(agent, env, batch_size, training_start, target_update_freq, 
+                max_episodes, max_steps, train_freq, backup_freq):
+
+  # agent.load_from_weights('model_200.h5')
+
+  avg_reward_tracker = AverageRewardTracker(100) 
+  logger = Logger()
+
+  for episode in range(max_episodes): # training loop
+    state = env.reset() # vector of 8
+
+    episode_reward = 0 # reward tracker
+
+    for step in range(max_steps): # limit number of steps
+      action = agent.select_action(state) # get action 
+      next_state, reward, done, info = env.step(action) # next step
+      episode_reward += reward # increment reward
+
+      if step == max_steps: # stop at max steps 
+        print(f"Episode reached the maximum number of steps. {max_steps}")
+        done = True
+
+      experience = Experience(state, action, reward, next_state, done) # create new experience object
+      agent.buffer.add(experience) # add experience to buffer
+
+      state = next_state # update state
+
+      if step % target_update_freq == 0: # update target weights every x steps 
+        print("Updating target model")
+        agent.update_target_weights()
+      
+      if (agent.buffer.length() >= training_start) & (step % train_freq == 0): # train agent every x steps
+        batch = agent.buffer.sample(batch_size)
+        inputs, targets = agent.calculate_inputs_and_targets(batch)
+        agent.train(inputs, targets)
+
+      if done: # stop if this action results in goal reached
+        break
+    
+    avg_reward_tracker.add(episode_reward)
+    average = avg_reward_tracker.get_average()
+
+    print(f"EPISODE {episode} finished in {step} steps, " )
+    print(f"epsilon {agent.epsilon}, reward {episode_reward}. ")
+    print(f"Average reward over last 100: {average} \n")
+    logger.log(episode, step, episode_reward, average)
+    if episode != 0 and episode % backup_freq == 0: # back up model every x steps 
+      backup_model(agent.model, episode)
+    
+    agent.epsilon_decay()
+  return agent.model, logger
+
+
 def backup_model(model, episode):
     backup_file = f"model_{episode}.h5"
     print(f"Backing up model to {backup_file}")
     model.save(backup_file)
 
-def plot(file_logger):
-    data = pd.read_csv(file_logger.file_name, sep=';')
+def plot(logger):
+    data = pd.read_csv(logger.file_name, sep=';')
     plt.figure(figsize=(20,10))
     plt.plot(data['average'])
     plt.plot(data['reward'])
@@ -50,3 +107,46 @@ def plot(file_logger):
     plt.ylabel('Reward')
     plt.legend(['Average reward', 'Reward'], loc='upper right')
     plt.savefig('reward_plot.png')
+
+def render_game(model_filename):
+  env = gym.make("LunarLander-v2")
+  trained_model = load_model(model_filename)
+
+  evaluation_max_episodes = 10
+  evaluation_max_steps = 1000
+
+  def get_q_values(model, state):
+      state = np.array(state)
+      return model.predict(input)[0]
+
+  def select_best_action(q_values):
+      return np.argmax(q_values)
+
+  rewards = []
+  for episode in range(evaluation_max_episodes):
+      state = env.reset()
+
+      episode_reward = 0
+
+      for step in range(evaluation_max_steps):
+          env.render()
+          q_values = get_q_values(trained_model, state)
+          action = select_best_action(q_values)
+          next_state, reward, done, info = env.step(action)
+
+          episode_reward += reward
+
+          if step == evaluation_max_steps:
+              print(f"Episode reached the maximum number of steps. {evaluation_max_steps}")
+              done = True
+
+          state = next_state
+
+          if done:
+              break
+
+      print(f"episode {episode} finished in {step} steps with reward {episode_reward}.")
+      rewards.append(episode_reward)
+
+  print(rewards)
+  print("Average reward: " + np.average(rewards))
--- lunarlander.py
+++ lunarlander.py
@@ -12,7 +12,7 @@ from collections import deque
 
 from ReplayBuffer import ReplayBuffer
 from Experience import Experience
-from Utils import FileLogger, AverageRewardTracker, plot, backup_model
+from Utils import Logger, AverageRewardTracker, plot, train_loop, render_game
 from DDQN import DDQN
 
 env = gym.make("LunarLander-v2")
@@ -38,53 +38,7 @@ backup_freq = 100
 agent = DDQN(state_space, action_space, learning_rate, 
   gamma, epsilon, min_epsilon, decay_rate, buffer_maxlen, reg_factor)
 
-agent.load_from_weights('model_200.h5')
+model, progress_data = train_loop(agent, env, batch_size, training_start,
+  target_update_freq, max_episodes, max_steps, train_freq, backup_freq)
 
-avg_reward_tracker = AverageRewardTracker(100) 
-file_logger = FileLogger()
-
-for episode in range(max_episodes): # training loop
-  episode_reward = 0 
-  state = env.reset() # vector of 8
-
-  total_reward = 0 # reward tracker
-
-  for step in range(1, max_steps + 1): # limit number of steps
-    action = agent.select_action(state) # get action 
-    next_state, reward, done, info = env.step(action) # next step
-    total_reward += reward # increment reward
-
-    if step == max_steps: # stop at max steps 
-      print(f"Episode reached the maximum number of steps. {max_steps}")
-      done = True
-
-    experience = Experience(state, action, reward, next_state, done) # create new experience object
-    agent.buffer.add(experience) # add experience to buffer
-
-    state = next_state # update state
-
-    if step % target_update_freq == 0: # update target weights every x steps 
-      print("Updating target model")
-      agent.update_target_weights()
-    
-    if (agent.buffer.length() >= training_start) & (step % train_freq == 0): # train agent every x steps
-      batch = agent.buffer.sample(batch_size)
-      inputs, targets = agent.calculate_inputs_and_targets(batch)
-      agent.train(inputs, targets)
-
-    if done: # stop if this action results in goal reached
-      break
-  
-  avg_reward_tracker.add(total_reward)
-  average = avg_reward_tracker.get_average()
-
-  print(f"EPISODE {episode} finished in {step} steps, " )
-  print(f"epsilon {agent.epsilon}, reward {total_reward}. ")
-  print(f"Average reward over last 100: {average} \n")
-  file_logger.log(episode, step, total_reward, average)
-  if episode != 0 and episode % backup_freq == 0: # back up model every x steps 
-    backup_model(agent.model, episode)
-  
-  agent.epsilon_decay()
-
-plot(file_logger)
+plot(progress_data)
